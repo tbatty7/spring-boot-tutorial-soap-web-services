6f4f251b98670ffcf74fc5245f3804dc03b4ca0a
Tim Batty <tbatty7@gmail.com>
2018-01-22 23:24:35 -0500

    Initial commit

---
+++ src/main/java/com/in28minutes/springboot/soap/web/services/example/student/Student.java
@@ -0,0 +1,56 @@
+package com.in28minutes.springboot.soap.web.services.example.student;
+
+public class Student {
+    private Long id;
+    private String name;
+    private String passportNumber;
+
+    public Student() {
+        super();
+    }
+
+    public Student(Long id, String name, String passportNumber) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.passportNumber = passportNumber;
+    }
+
+    public Student(String name, String passportNumber) {
+        super();
+        this.name = name;
+        this.passportNumber = passportNumber;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassportNumber() {
+        return passportNumber;
+    }
+
+    public void setPassportNumber(String passportNumber) {
+        this.passportNumber = passportNumber;
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        return String.format("Student [id=%s, name=%s, passportNumber=%s]", id, name, passportNumber);
+    }
+}
+
